a30edf467717550851ae602804ff0cf32a5ed6dd
Thalles <thallesmlopes@gmail.com>
2024-09-30 20:22:40 -0300

    Work anchor

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 
 export default function Work({ projects }) {
   return (
-    <section className={`${styles.work} section highlight`}>
+    <section id="work" className={`${styles.work} section highlight`}>
       {projects?.length !== 0 && projects.map((project) => <Project key={project.id} project={project} />)}
     </section>
   );
