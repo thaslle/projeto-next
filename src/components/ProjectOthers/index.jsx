d3421404a6b2a920dc577aced394eb2e9146789f
Thalles <thallesmlopes@gmail.com>
2024-10-01 20:01:23 -0300

    Work & Experiments

--- src/components/ProjectOthers/index.jsx
+++ src/components/ProjectOthers/index.jsx
@@ -3,10 +3,10 @@ import styles from './styles.module.scss';
 
 export const ProjectOthers = ({ projects }) => {
   return (
-    <section className={styles.otherProjects}>
+    <section className={`${styles.otherProjects} section`}>
       <h2>See more</h2>
 
-      <div className={`${styles.projects} section`}>
+      <div className={styles.projects}>
         {projects.length !== 0 && projects.map((project) => <Project key={project.id} project={project} />)}
       </div>
     </section>
